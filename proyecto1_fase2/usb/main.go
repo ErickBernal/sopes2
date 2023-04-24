6986534faecb5a9c0114733e80480ebc67f716e3
ErickBernal <pilloubool@gmail.com>
2023-04-23 19:40:24 -0600

    agregando reguridad al sevicio USB

--- proyecto1_fase2/usb/main.go
+++ proyecto1_fase2/usb/main.go
@@ -39,28 +39,38 @@ func leerLogs() {
 
 func main() {
 	var x int
-	for {
-		fmt.Println("Seleccione un opcion")
-		fmt.Println("	1. Montar usb")
-		fmt.Println("	2. Desmontar usb")
-		fmt.Println("	3. Copiar archivos")
-		fmt.Println("	4. Mostrar archivo Logs")
-		fmt.Println("	9. salir")
-		fmt.Scanf("%d", &x)
-		if x == 1 {
-			execComand("./mount.sh")
-			fmt.Println("<Mount usb>")
-		} else if x == 2 {
-			execComand("./umount.sh")
-			fmt.Println("(Umount usb)")
-		} else if x == 3 {
-			execComand("./log.sh")
-			fmt.Println("**LOG**")
-		} else if x == 4 {
-			leerLogs()
-		} else if x == 9 {
-			break
+	var usu string
+	var pwd string
+
+	fmt.Println("Ingrese usuario:")
+	fmt.Scanf("%s", &usu)
+	fmt.Scanf("%s", &pwd)
+	if usu == "erick" && pwd == "123" {
+		for {
+			fmt.Println("Seleccione un opcion")
+			fmt.Println("	1. Montar usb")
+			fmt.Println("	2. Desmontar usb")
+			fmt.Println("	3. Copiar archivos")
+			fmt.Println("	4. Mostrar archivo Logs")
+			fmt.Println("	9. salir")
+			fmt.Scanf("%d", &x)
+			if x == 1 {
+				execComand("./mount.sh")
+				fmt.Println("<Mount usb>")
+			} else if x == 2 {
+				execComand("./umount.sh")
+				fmt.Println("(Umount usb)")
+			} else if x == 3 {
+				execComand("./log.sh")
+				fmt.Println("**LOG**")
+			} else if x == 4 {
+				leerLogs()
+			} else if x == 9 {
+				break
+			}
 		}
+	} else {
+		fmt.Println("**Error al ingresar usuario**")
 	}
 
 }
